2b8438187d7b7ddf049517a0b499c84d4eb4ac61
Entwicklerfuchs26 <jonas@hofpause.info>
2026-07-20 22:11:35 +0200

    feat: core/base.nix + hosts/nexus/host-config.nix

---
+++ hosts/nexus/host-config.nix
@@ -0,0 +1,66 @@
+# Host-spezifische Konfiguration – NICHT ins public repo committen ohne Bereinigung
+# (Hostname, MACs, UUIDs, interne Domains, Zertifikatspfade)
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "nexus";
+
+  networking.interfaces.enp5s0 = {
+    wakeOnLan.enable = true;
+  };
+
+  security.sudo.extraRules = [{
+    users = [ "fuchs" ];
+    commands = [{
+      command = "/run/current-system/sw/bin/shutdown";
+      options = [ "NOPASSWD" ];
+    }];
+  }];
+
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = true;
+
+  # Sternenhof Root-CA – Pfad muss auf lokale certs/ im Config-Repo zeigen
+  # security.pki.certificateFiles = [ ./certs/sternenhof-ca-2026.crt ];
+
+  networking.hosts = {
+    "192.168.1.26" = [
+      "cloud.sternenhof.space"
+      "n8n.sternenhof.space"
+      "media.sternenhof.space"
+      "photos.sternenhof.space"
+      "tasks.sternenhof.space"
+      "home.sternenhof.space"
+      "ntfy.sternenhof.space"
+      "zugang.sternenhof.space"
+      "darwin26.sternenhof.space"
+      "sojus.sternenhof.space"
+    ];
+  };
+
+  # Bose Bluetooth-Headset: A2DP-Profil erzwingen
+  services.pipewire.wireplumber.extraConfig."51-bluez-bose-a2dp" = {
+    "monitor.bluez.rules" = [
+      {
+        matches = [
+          { "device.name" = "bluez_card.7C_96_D2_77_56_7E"; }
+        ];
+        actions = {
+          update-props = {
+            "bluez5.auto-connect" = [ "a2dp_sink" ];
+            "device.profile" = "a2dp-sink";
+          };
+        };
+      }
+    ];
+  };
+
+  # 1 TB Datenfestplatte (ext4)
+  fileSystems."/mnt/data" = {
+    device = "/dev/disk/by-uuid/299df179-7040-42b8-a6f0-548247cc82f6";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" "x-systemd.automount" ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8080 8081 8888 7777 ];
+}
---
+++ modules/core/base.nix
@@ -0,0 +1,85 @@
+{ config, pkgs, ... }:
+
+{
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
+
+  hardware.uinput.enable = true;
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  services.printing.enable = true;
+  # Drucker-spezifische Konfiguration gehört ins eigene Modul (modules/hardware/printing.nix)
+
+  # Audio mit Pipewire
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # Gerätespezifische Wireplumber-Regeln (z. B. Bluetooth-Headset-Profile)
+    # → in host-config.nix oder eigenem Modul definieren
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Experimental = true;
+      ReconnectAttempts = 7;
+      ReconnectIntervals = "1,2,4,8,16,32,64";
+    };
+  };
+
+  services.udev.packages = [ pkgs.openrgb ];
+  boot.kernelModules = [ "btusb" "i2c-dev" ];
+
+  programs.nix-ld.enable = true;
+
+  users.groups.i2c = {};
+  services.udev.extraRules = ''
+    KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
+    # Intel AX200 Bluetooth USB-Autosuspend deaktivieren (verhindert zufälliges Verschwinden)
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0029", ATTR{power/autosuspend}="-1"
+  '';
+
+  services.usbmuxd.enable = true;
+  services.udisks2.enable = true;
+  services.gvfs.enable = true;
+
+  boot.supportedFilesystems = [ "ntfs" ];
+  # Kernel-exFAT deaktiviert → fuse-exfat übernimmt (uid/gid funktioniert)
+  boot.blacklistedKernelModules = [ "exfat" ];
+  environment.systemPackages = with pkgs; [ udisks2 exfat ];
+
+  environment.etc."udisks2/mount_options.conf".text = ''
+    [defaults]
+    exfat_defaults=uid=$UID,gid=$GID,umask=002
+    exfat_allow=uid=$UID,gid=$GID,umask,dmask,fmask,iocharset,errors
+  '';
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("wheel") &&
+        action.id.indexOf("org.freedesktop.udisks2.") == 0
+      ) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
+  # system.stateVersion nicht ändern ohne Migration (nixos-rebuild switch kann Datenverlust verursachen)
+  system.stateVersion = "25.11";
+}
