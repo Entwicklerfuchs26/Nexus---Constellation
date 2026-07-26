ef9ac227cc8e8808613f48147c18f58ee7eafe33
Sojus-Agent <sojus@nexus>
2026-07-27 00:33:38 +0200

    pre-rebuild: docker: nvidia-assertion suppressed

--- modules/software/docker.nix
+++ modules/software/docker.nix
@@ -1,16 +1,13 @@
 { config, pkgs, ... }:
-
 {
   hardware.nvidia-container-toolkit.enable = true;
-
+  hardware.nvidia-container-toolkit.suppressNvidiaDriverAssertion = true;
   virtualisation.docker = {
     enable = true;
     autoPrune.enable = true;
     autoPrune.dates = "weekly";
   };
-
   # users.users.<username>.extraGroups = [ "docker" ] → hosts/nexus/host-config.nix
-
   environment.systemPackages = with pkgs; [
     docker-compose
   ];
